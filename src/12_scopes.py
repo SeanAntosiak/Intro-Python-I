8b0cd9e26fe5c212746db5e05f9d8f5cad99707d
Sean Antosiak <52640577+SeanAntosiak@users.noreply.github.com>
2020-01-13 15:54:47 -0500

    Update 12_scopes.py

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,7 +5,8 @@
 x = 12
 
 def change_x():
-    x = 99
+     global x 
+     x = 99
 
 change_x()
 
@@ -16,9 +17,11 @@ print(x)
 # This nested function has a similar problem.
 
 def outer():
+    global y
     y = 120
 
     def inner():
+        global y
         y = 999
 
     inner()
